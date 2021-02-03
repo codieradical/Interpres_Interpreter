0edb94ee910cacc15049a5eb33f2385aa0601df2
Alex231 <git@alex231.com>
2021-02-03 07:21:23 +0000

    Added workspace to debugger.

--- Debugging/ASTTest.cs
+++ Debugging/ASTTest.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Interpreter.IO;
 
 namespace Interpreter.Debugging
 {
@@ -13,8 +14,8 @@ namespace Interpreter.Debugging
     {
         public static void Debug(string[] args)
         {
-            TokenizerService tokenizerService = new TokenizerService(new ConsoleCommandTokenizer());
-
+            TokenizerService tokenizerService = new TokenizerService();
+                
             while (true)
             {
                 string input = Console.ReadLine();
@@ -22,16 +23,18 @@ namespace Interpreter.Debugging
                 try
                 {
                     var tokens = tokenizerService.GetTokens(input);
-                    //foreach (AbstractToken token in tokens)
+                    //foreach (object token in subtreeTokens)
                     //{
-                    //    Console.WriteLine("token: " + token.GetInputString() + " " + token.GetType().ToString());
+                    //    Console.WriteLine("token: " + token.GetType().ToString());
                     //    if (token is ValueToken)
                     //    {
                     //        Console.WriteLine("Value: " + ((ValueToken)token).Value);
                     //    }
                     //}
 
-                    Console.WriteLine("ans: " + new AbstractSyntaxTree(tokens.Select(token => (object) token).ToList()).GetValue());
+                    Workspace workspace = new LocalFileWorkspace(null, new string[0]);
+
+                    Console.WriteLine("ans: " + new AbstractSyntaxTree(tokens.Select(token => (object) token).ToList(), workspace).GetValue());
                 } catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
